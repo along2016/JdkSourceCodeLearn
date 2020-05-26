529ff9ea9fb9ca708934e011c3363dd81ea61a59
wangjiliang <845506513@qq.com>
2020-05-26 23:03:00 +0800

    加注释

--- src/com/wjl/java/util/concurrent/atomic/AtomicBoolean.java
+++ src/com/wjl/java/util/concurrent/atomic/AtomicBoolean.java
@@ -44,6 +44,9 @@ import sun.misc.Unsafe;
  * updated flags, and cannot be used as a replacement for a
  * {@link java.lang.Boolean}.
  *
+ * 一个可以自动更新的布尔值。有关原子变量属性的描述，请参阅 java.util.concurrent.atomic 包规范。
+ * AtomicBoolean 类用于原子更新标识等应用程序中，不能作为 java.lang.Boolean 的替代。
+ *
  * @since 1.5
  * @author Doug Lea
  */
@@ -64,6 +67,7 @@ public class AtomicBoolean implements java.io.Serializable {
 
     /**
      * Creates a new {@code AtomicBoolean} with the given initial value.
+     * 用给定的初始值创建一个 AtomicBoolean 对象
      *
      * @param initialValue the initial value
      */
@@ -73,6 +77,7 @@ public class AtomicBoolean implements java.io.Serializable {
 
     /**
      * Creates a new {@code AtomicBoolean} with initial value {@code false}.
+     * 用初始值 false 创建一个 AtomicBoolean 对象
      */
     public AtomicBoolean() {
     }
@@ -89,6 +94,7 @@ public class AtomicBoolean implements java.io.Serializable {
     /**
      * Atomically sets the value to the given updated value
      * if the current value {@code ==} the expected value.
+     * 如果当前值与期望值相等，则自动将该值设置为给定的更新值。
      *
      * @param expect the expected value
      * @param update the new value
@@ -106,8 +112,10 @@ public class AtomicBoolean implements java.io.Serializable {
      * if the current value {@code ==} the expected value.
      *
      * <p><a href="package-summary.html#weakCompareAndSet">May fail
-     * spuriously and does not provide ordering guarantees</a>, so is
+     * spuriously（不合逻辑的） and does not provide ordering guarantees</a>, so is
      * only rarely an appropriate alternative to {@code compareAndSet}.
+     *
+     * weakCompareAndSet 可能会错误地失败，并且不提供排序保证，因此很少是 compareAndSet 的合适替代。
      *
      * @param expect the expected value
      * @param update the new value
