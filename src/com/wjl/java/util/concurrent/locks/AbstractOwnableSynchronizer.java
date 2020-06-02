09c7efbe3eeb4ed1272c9e518b48bfe83fd2ccc4
wangjiliang <845506513@qq.com>
2020-06-02 22:31:15 +0800

    加注释

--- src/com/wjl/java/util/concurrent/locks/AbstractOwnableSynchronizer.java
+++ src/com/wjl/java/util/concurrent/locks/AbstractOwnableSynchronizer.java
@@ -44,6 +44,10 @@ package java.util.concurrent.locks;
  * appropriately maintained values to help control and monitor access
  * and provide diagnostics.
  *
+ * 一种同步器，它可能只属于一个线程。该类提供了创建锁和相关同步器的基础，这些同步器可能包含所有权的概念。
+ * AbstractOwnableSynchronizer 类本身并不管理和使用信息。
+ * 但是，它的子类和工具类可以使用适当维护的值来帮助控制和监视访问并提供诊断。
+ *
  * @since 1.6
  * @author Doug Lea
  */
@@ -60,6 +64,7 @@ public abstract class AbstractOwnableSynchronizer
 
     /**
      * The current owner of exclusive mode synchronization.
+     * 独占模式同步器的当前所有者。
      */
     private transient Thread exclusiveOwnerThread;
 
@@ -68,6 +73,11 @@ public abstract class AbstractOwnableSynchronizer
      * A {@code null} argument indicates that no thread owns access.
      * This method does not otherwise impose any synchronization or
      * {@code volatile} field accesses.
+     *
+     * 设置当前拥有独占访问权的线程。
+     * null 参数表示没有线程拥有访问权。
+     * 此方法不强制任何 synchronization 或 volatile 字段访问。
+     *
      * @param thread the owner thread
      */
     protected final void setExclusiveOwnerThread(Thread thread) {
